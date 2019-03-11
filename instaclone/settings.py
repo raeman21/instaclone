bbce1942af9a85a5dacc1b914eaea9f3cf115393
raeman21 <mandelar94@gmail.com>
2019-03-11 11:39:21 +0300

    return welcome url

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'photos'
-    'bootstrap4'
+    'photos',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- photos/views.py
+++ photos/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required.
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse( 'welcome.html')
 
 @login_required(login_url='/accounts/login/')
+

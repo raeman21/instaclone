b3fbecb77d4343fb49ec04d9ef57ef2340731f43
raeman21 <mandelar94@gmail.com>
2019-03-11 12:59:32 +0300

    add update and save profile functions

--- photos/models.py
+++ photos/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='home/')
+    bio = models.CharField(max_length=50)
+
+    def save_profile(self):
+        '''
+        This is a function that will save the profile
+        '''
+        self.save()
+
+    def update_profile(self):
+        '''
+        This is a function that will update the profile
+        '''
+        self.update()
+
+    
 
 class Image(models.Model):
+    image = models.ImageField(upload_to = 'home/')
     image_name = models.CharField(max_length=30)
     image_caption = models.TextField()
     profile = models.ForeignKey(profile)

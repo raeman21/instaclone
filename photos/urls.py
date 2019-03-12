af8aa2454b770358e799e1a80abe9c57b88fed3d
raeman21 <mandelar94@gmail.com>
2019-03-12 03:38:04 +0300

    add home view

--- photos/urls.py
+++ photos/urls.py
@@ -4,5 +4,14 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.welcome,name= 'welcome'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^$',views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+    url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
+    url(r'^upload/$', views.upload_image, name='upload_image'),
+    url(r'^accounts/edit/',views.edit_profile, name='edit_profile'),
+    url(r'^image/(?P<image_id>\d+)', views.single_image, name='single_image'),
+    url(r'^search/', views.search, name='search')
+]
+
 ]
--- photos/views.py
+++ photos/views.py
@@ -2,8 +2,23 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+@login_required(login_url='/')
+def home(request):
+    images = Image.get_all_images()
+    
+    return render(request, 'index.html', {'images':images})
 
-# @login_required(login_url='/accounts/login/')
+@login_required(login_url='/accounts/login/')
+def new_pic(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('NewsToday')
 
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_pic.html', {"form": form})

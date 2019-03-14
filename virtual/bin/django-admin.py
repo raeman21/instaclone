2ae4244f0dcb16bc4c9755213cd718a224bf886a
raeman21 <mandelar94@gmail.com>
2019-03-14 04:27:48 +0300

    destroy file structure and build from scratch

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/raeman94/MS-Python-Pre-work/python-project/Django/instaclone/virtual/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

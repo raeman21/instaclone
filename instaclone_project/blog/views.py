1a8ea984d5c64f0bf493d34b30a06ce95f0a4476
raeman21 <mandelar94@gmail.com>
2019-03-14 04:37:48 +0300

    add views

--- instaclone_project/blog/views.py
+++ instaclone_project/blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+
+
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'home.html' 
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    
+   
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About'} )

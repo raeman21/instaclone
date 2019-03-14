9eba44cba05cd0a557c92dbb6ee02d48eebd045f
raeman21 <mandelar94@gmail.com>
2019-03-14 12:38:32 +0300

    fixed deploy error

--- instaclone_project/settings.py
+++ instaclone_project/settings.py
@@ -164,7 +164,7 @@ MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 django_heroku.settings(locals())
